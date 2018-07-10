0b0f8115a73a2c507bfcfe2fec58008924454447
Lord丶Y <421492867@qq.com>
2018-07-10 22:08:51 +0800

    Graph ADT

--- data_structure/data_structure/Graph.hpp
+++ data_structure/data_structure/Graph.hpp
@@ -8,7 +8,48 @@
 
 #ifndef Graph_hpp
 #define Graph_hpp
+const int MAX_NODE =
 
 #include <stdio.h>
 
 #endif /* Graph_hpp */
+//边
+struct EdgeNode{
+    int adjvex;
+    int weight;
+    EdgeNode *next;
+};
+
+struct VertexNode{
+    char data;
+    EdgeNode *firstedge;
+};
+
+class Graph{
+private:
+    int numVertexes;
+    int numEdges;
+    VertexNode *nodes;
+public:
+    Graph(int size){
+        numVertexes = 0;
+        numEdges = 0;
+        nodes = new VertexNode[size];
+        for(int i = 0; i < size; i++){
+            nodes[i].firstedge = NULL;
+        }
+    }
+    void createGraph(Graph &g);
+    int locateVex(VertexNode node);
+    int getVex(int index);
+    bool putVex(int index, int value);
+    EdgeNode firstAdjVex(VertexNode node);
+    EdgeNode nextAdjVex(VertexNode v, VertexNode w);
+    bool insertVex(VertexNode node);
+    bool deleteVex(VertexNode node);
+    bool insertArc(VertexNode v, VertexNode w);
+    bool deleteArc(VertexNode v, VertexNode w);
+    void DFSTraverse();
+    void HFSTraverse();
+};
+
