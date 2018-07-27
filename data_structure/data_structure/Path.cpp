9feae7fd14222728934e39c8a7b88d063305b7c1
Lord丶Y <421492867@qq.com>
2018-07-27 10:26:53 +0800

    path

---
+++ data_structure/data_structure/Path.cpp
@@ -0,0 +1,35 @@
+//
+//  Path.cpp
+//  data_structure
+//
+//  Created by ysp on 2018/7/27.
+//  Copyright © 2018年 Lord、y. All rights reserved.
+//
+
+#include "Path.hpp"
+#include <iostream>
+using namespace std;
+void Graph_Path::createGraph(){
+    cout << "请输入结点数量" << endl;
+    cin >> this -> numVertexes;
+    cout << "请输入结点" << endl;
+    string node;
+    cin >> node;
+    for(int i = 0; i < node.length(); i++){
+        this -> vex[i] = node[i];
+    }
+    for(int i = 0; i < this -> numVertexes; i++){
+        for (int j = 0; j < this -> numVertexes; j++) {
+            this -> arc[i][j] = P_INFINITY;
+        }
+    }
+    cout << "请输入边数量" << endl;
+    cin >> this -> numEdges;
+    for(int i = 0; i < this -> numEdges; i++){
+        cout << "请输入边序对及权重" << endl;
+        int start, end, weight;
+        cin >> start >> end >> weight;
+        this -> arc[start][end] = weight;
+        this -> arc[end][start] = weight;
+    }
+}
---
+++ data_structure/data_structure/Path.hpp
@@ -0,0 +1,28 @@
+//
+//  Path.hpp
+//  data_structure
+//
+//  Created by ysp on 2018/7/27.
+//  Copyright © 2018年 Lord、y. All rights reserved.
+//
+
+#ifndef Path_hpp
+#define Path_hpp
+
+#include <stdio.h>
+
+#endif /* Path_hpp */
+const int P_INFINITY = 65535;
+const int P_DEFAULT_SIZE = 30;
+class Graph_Path{
+private:
+    char vex[P_DEFAULT_SIZE];
+    int arc[P_DEFAULT_SIZE][P_DEFAULT_SIZE];
+    int numVertexes, numEdges;
+public:
+    Graph_Path(){
+        numVertexes = 0;
+        numEdges = 0;
+    };
+    void createGraph();
+};
