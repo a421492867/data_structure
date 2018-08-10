473b86720ea00a4387371f3fd6669a5ec865e685
Lord丶Y <421492867@qq.com>
2018-08-10 09:54:28 +0800

    funcs

--- data_structure/data_structure/BST.hpp
+++ data_structure/data_structure/BST.hpp
@@ -12,3 +12,19 @@
 #include <stdio.h>
 
 #endif /* BST_hpp */
+struct BSTNode{
+    int data;
+    BSTNode *left;
+    BSTNode *right;
+    BSTNode():left(NULL),right(NULL){};
+};
+
+class BST{
+private:
+    BSTNode *root;
+public:
+    bool insert(int key);
+    int search(int key);
+    bool deleteBST(int key);
+    
+};
