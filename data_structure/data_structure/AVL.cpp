97c1c1e9522776cb779cf866031240ef3bceeb8f
Lord丶Y <421492867@qq.com>
2018-08-14 10:35:54 +0800

    .

---
+++ data_structure/data_structure/AVL.cpp
@@ -0,0 +1,9 @@
+//
+//  AVL.cpp
+//  data_structure
+//
+//  Created by ysp on 2018/8/14.
+//  Copyright © 2018年 Lord、y. All rights reserved.
+//
+
+#include "AVL.hpp"
---
+++ data_structure/data_structure/AVL.hpp
@@ -0,0 +1,37 @@
+//
+//  AVL.hpp
+//  data_structure
+//
+//  Created by ysp on 2018/8/14.
+//  Copyright © 2018年 Lord、y. All rights reserved.
+//
+
+#ifndef AVL_hpp
+#define AVL_hpp
+
+#include <stdio.h>
+
+#endif /* AVL_hpp */
+
+struct AVLNode{
+    int data;
+    int bf;
+    AVLNode *left;
+    AVLNode *right;
+    AVLNode():left(NULL),right(NULL){};
+};
+
+class AVL{
+private:
+    AVLNode *root;
+public:
+    AVL(){
+        root = new AVLNode();
+    }
+    
+    void r_rotate(AVL *&p);
+    void l_rotate(AVL *&p);
+    void leftBalance(AVL *&t);
+    void rightBalance(AVL *&t);
+    bool insert(int data);
+};
